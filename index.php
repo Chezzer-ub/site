51dea5e99baf274252bc7fa2fca0b0cdeeca0c52
Chezzer <ryanhamiltoncherry@gmail.com>
2021-05-04 18:34:49 +1200

    update

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
             <div class="row">
                 <div class="col-sm-6">
                     <a href="https://sapphireadventures.net" target="_blank">
-                        <div class="project img-wrap" hover-color="rgba(66, 187, 242, .25)" style="background:url(asset/img/sapphireadventures.net.png);background-size: cover;"></div>
+                        <div class="project img-wrap" hover-color="rgba(66, 187, 242, .5)" style="background:url(asset/img/sapphireadventures.net.png);background-size: cover;"></div>
                     </a>
                 </div>
             </div>
