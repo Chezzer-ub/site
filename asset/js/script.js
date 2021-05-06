066b1dddc2c33eaa35cb7b0e181328b5b4503b83
Ryan <46093784+Chezzer-ub@users.noreply.github.com>
2021-05-07 09:07:10 +1200

    Update script.js

--- asset/js/script.js
+++ asset/js/script.js
@@ -80,7 +80,7 @@ function updateStats() {
                 $("#activity").append(`<p><span><i class="fas fa-gamepad"></i> ${item.name}</span></p>`);
             }
         })
-        $(".status").fadeIn()
+        $(".status").css("opacity", 1)
     })
 }
 
