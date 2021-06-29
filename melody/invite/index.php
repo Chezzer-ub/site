d53b62b00c530907dfcacc81c80b2cf97aa055ef
Ryan <46093784+Chezzer-ub@users.noreply.github.com>
2021-06-29 22:34:11 +1200

    downgrade

--- melody/invite/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-header("location: https://discord.com/oauth2/authorize?client_id=793338351145975808&scope=bot&permissions=3525696");
-?>
--- melody/views/error/exist.php
+++
@@ -1,5 +0,0 @@
-<div style=" display: flex; justify-content: center; align-items: center; position: relative; top: 50%; left: 50%; transform: translateX(-50%) translateY(200%); ">
-    <div class="text-center text-danger"> <i class="fal fa-times-hexagon fa-5x"></i>
-        <h1 class="mt-4">Can't find that page.</h1>
-    </div>
-</div>
