b894e5276e0fe37b20ed3b49bdaf09439cf7a6b3
Ryan <46093784+Chezzer-ub@users.noreply.github.com>
2021-08-03 22:29:24 +1200

    add php

--- api/form.php
+++ api/form.php
@@ -1,2 +1,4 @@
 <?php
-print_r($_GET);
+if ($_POST['name'] && $_POST['contact'] && $_POST['subject'] && $_POST['message']) {
+    mail("ryanhamiltoncherry@gmail.com", $_POST['subject'], $_POST['message'], "From: {$_POST['contact']}");
+}
