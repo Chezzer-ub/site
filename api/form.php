e2498063f7e0730cdd7b72305cf7e7c471d940f1
Ryan <46093784+Chezzer-ub@users.noreply.github.com>
2021-08-03 22:34:58 +1200

    add php

--- api/form.php
+++ api/form.php
@@ -1,4 +1,10 @@
 <?php
+$res = new stdClass;
 if ($_POST['name'] && $_POST['contact'] && $_POST['subject'] && $_POST['message']) {
     mail("ryanhamiltoncherry@gmail.com", $_POST['subject'], $_POST['message'], "From: {$_POST['contact']}");
+    $res->success = true;
+} else {
+    $res->error = "Please enter all fields.";
 }
+header("content-type: application/json");
+echo json_encode($res);
---
+++ assets/js/home/contact.js
@@ -0,0 +1,14 @@
+$("#contact-form").on("submit", () => {
+    $.post("https://chezzer.dev/api/form.php", {
+        name: $("[name=name]").val(),
+        contact: $("[name=contact]").val(),
+        subject: $("[name=subject]").val(),
+        message: $("[name=message]").val()
+    }, (res) => {
+        if (res.success) {
+            $("#res").val("Sent, thank you for your message.")
+        } else {
+            $("#res").html(res.error);
+        }
+    })
+})
