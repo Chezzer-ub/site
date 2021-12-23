c30e1b8cf10274f7aa3e23fd24cb0e5a3eb15263
chezzzer <46093784+chezzzer@users.noreply.github.com>
2021-12-23 13:54:07 +1300

    update

--- assets/js/home/contact.js
+++ assets/js/home/contact.js
@@ -1,4 +1,6 @@
 $("#contact-form").submit(() => {
+    $("#contact-form [type='submit']").addClass("disabled");
+    $("#res").removeClass("text-green-500")
     $.post("https://chezzer.dev/api/form.php", {
         name: $("[name=name]").val(),
         contact: $("[name=contact]").val(),
@@ -8,7 +10,9 @@ $("#contact-form").submit(() => {
         if (res.success) {
             $("#res").html("Sent, thank you for your message.")
         } else {
+            $("#contact-form [type='submit']").removeClass("disabled");
             $("#res").html(res.error);
+            $("#res").addClass("text-green-500");
         }
     })
 })
